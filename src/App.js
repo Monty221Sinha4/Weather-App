34528c01c6003a996c681a2c87802d1b034b634e
Monty Sinha <monty.sinha2013@live.co.uk>
2019-04-21 21:12:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+
+
+class title extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Weather Finder</h1>
+      </div>
+    );
+  }
+};
+
+export default App;
