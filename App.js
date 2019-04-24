b5e2fd5c898f17c16c29c4616e0d90782f87c4d0
Monty Sinha <monty.sinha2013@live.co.uk>
2019-04-24 17:42:47 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import react from "react";
+import Title from "compoenets/Titles";
+import form from "compoenets/Form";
+import weather from "compoenets/weather";
+
+Const API_KEY="c0c97585c2mshf72be85af0bb3e6p172e9bjsn962caac36c37";
+
+class App extends React.Component{
+  constructor(){
+    
+  }
+  getweather()=>{
+
+  }
+  render(){
+    return(
+      <div>
+         </title>
+         </form>
+         </weather>
+      </div>
+    );
+  }
+};
+
+export default App;
