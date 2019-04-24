6fa495e0226225e38eea15ddb3a6486a0fac8045
Monty Sinha <monty.sinha2013@live.co.uk>
2019-04-24 17:53:14 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import weather from "compoenets/weather";
 Const API_KEY="c0c97585c2mshf72be85af0bb3e6p172e9bjsn962caac36c37";
 
 class App extends React.Component{
-  constructor(){
-    
-  }
-  getweather()=>{
 
-  }
+getweather = asnyc () =>{
+  const api_call=await fetch('https://api.openweathermap.org/data/2.5/weather?q=mancherster,uk&appid=${API_KEY&}&units=metrics');
+  const data=await api_call.json();
+  console.log(data);
+}
+
   render(){
     return(
       <div>
          </title>
-         </form>
+       <Form getweather={This.getweather}/>
          </weather>
       </div>
     );
